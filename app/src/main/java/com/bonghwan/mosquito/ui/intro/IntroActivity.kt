942a292cb629752faf4f19444190a7575aad17c8
YeomDongBin <yeomdongbin@naver.com>
2023-11-11 20:30:19 +0900

    2023.11.11(토) / ;;

--- app/src/main/java/com/bonghwan/mosquito/ui/intro/IntroActivity.kt
+++ app/src/main/java/com/bonghwan/mosquito/ui/intro/IntroActivity.kt
@@ -81,6 +81,7 @@ class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro
 
         viewModel.error.observe(this) {
             if (it != null) Log.d(TAG, it.toString())
+            isLoggedIn = false
         }
 
         viewModel.kakaoProfile.observe(this) {
@@ -203,7 +204,6 @@ class IntroActivity : BaseActivity<ActivityIntroBinding>(R.layout.activity_intro
             UserApiClient.instance.loginWithKakaoTalk(this) { token, error ->
                 if (error != null) {
                     Log.e(TAG, "카카오톡으로 로그인 실패", error)
-
                     // 사용자가 카카오톡 설치 후 디바이스 권한 요청 화면에서 로그인을 취소한 경우,
                     // 의도적인 로그인 취소로 보고 카카오계정으로 로그인 시도 없이 로그인 취소로 처리 (예: 뒤로 가기)
                     if (error is ClientError && error.reason == ClientErrorCause.Cancelled) {
